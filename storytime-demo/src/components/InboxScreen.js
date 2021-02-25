88a2c1ff5f8a41eda30cde2b4532983294000e84
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-02-25 11:48:40 -0800

    fix code and add storyshots

--- storytime-demo/src/components/InboxScreen.js
+++ storytime-demo/src/components/InboxScreen.js
@@ -1,30 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { action } from '@storybook/addon-actions'
 import TaskList from './TaskList'
 
-const actions = {
-  onPinTask: action('onPinTask'),
-  onArchiveTask: action('onArchiveTask')
-}
+function InboxScreen({ error, tasks, onPinTask, onArchiveTask }) {
+  const events = {
+    onPinTask,
+    onArchiveTask,
+  };
 
-const task = {
-  id: '1',
-  title: 'Test Task',
-  state: 'TASK_INBOX',
-  updatedAt: new Date(2018, 0, 1, 9, 0)
-}
-
-const defaultTasks = [
-  { ...task, id: '1', title: 'Task 1' },
-  { ...task, id: '2', title: 'Task 2' },
-  { ...task, id: '3', title: 'Task 3' },
-  { ...task, id: '4', title: 'Task 4' },
-  { ...task, id: '5', title: 'Task 5' },
-  { ...task, id: '6', title: 'Task 6' }
-]
-
-function InboxScreen({ error }) {
   if (error) {
     return (
       <div className="page lists-show">
@@ -44,7 +27,7 @@ function InboxScreen({ error }) {
           <span className="title-wrapper">Taskbox</span>
         </h1>
       </nav>
-      <TaskList tasks={defaultTasks} {...actions} />
+      <TaskList tasks={tasks} {...events} />
     </div>
   )
 }
--- storytime-demo/src/components/InboxScreen.stories.js
+++ storytime-demo/src/components/InboxScreen.stories.js
@@ -2,11 +2,22 @@ import React from 'react';
 import InboxScreen from './InboxScreen';
 import { storiesOf } from '@storybook/react';
 
+import { task, actions } from './Task.stories';
+
+export const defaultTasks = [
+  { ...task, id: '1', title: 'Task 1' },
+  { ...task, id: '2', title: 'Task 2' },
+  { ...task, id: '3', title: 'Task 3' },
+  { ...task, id: '4', title: 'Task 4' },
+  { ...task, id: '5', title: 'Task 5' },
+  { ...task, id: '6', title: 'Task 6' },
+];
+
 export default { 
   title: 'InboxScreen',
   decorators: [ story => <div style={{ padding: '3rem' }}>{story()}</div> ]
 }
 
 storiesOf('InboxScreen', module)
-  .add('basic', () => <InboxScreen />)
-  .add('error', () => <InboxScreen error="Something" />);
+  .add('basic', () => <InboxScreen tasks={defaultTasks} {...actions} />)
+  .add('error', () => <InboxScreen error="Something" tasks={defaultTasks} {...actions} />);
